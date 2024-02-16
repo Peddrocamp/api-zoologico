16a9c2f3f3f2628451e015b0dde0d101da030f68
Pedro Campos <pedro1978campos@icloud.com>
2024-02-16 15:24:26 -0300

    Corrigindo erros

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import cors from 'cors';
+import { Zoologico } from './model/Zoologico';
+import { Atracao } from './model/Atracao';
+import { Habitat } from './model/Habitat';
 
 const server = express();
-const port = 
-
-;
+const port = 3000;
 
 server.use(express.json());
 server.use(cors());
@@ -12,6 +13,27 @@ server.use(cors());
 server.get('/', (req,res) =>{
  res.json('Olá mundo!!');
 })
+
+server.post('/habitat', (req, res) => {
+    const { nome, animais } = req.body;
+    const habitat = new Habitat(nome, animais);
+    console.log(habitat);
+    res.status(200).json('Habitat criado');
+});
+
+server.post('/atracao', (req, res) => {
+    const { nome, habitat } = req.body;
+    const atracao = new Atracao(nome, habitat);
+    console.log(atracao);
+    res.status(200).json('Atração criada');
+});
+
+server.post('/zoologico', (req, res) => {
+    const { nome, atracao } = req.body;
+    const zoo = new Zoologico(nome, atracao);
+    console.log(zoo);
+    res.status(200).json('Zoológico criado');
+});
 
  server.listen(port, ()  => {  
  console.log('Servidor rodando em http://localhost:${port}');
--- src/model/Zoologico.ts
+++ src/model/Zoologico.ts
@@ -1,12 +1,13 @@
+import { Atracao } from "./Atracao";
 
 
 export class Zoologico {
     private nome: string;
-    private lista_de_atrações: Array<Atracao>;
+    private lista_de_atracoes: Array<Atracao>;
 
-    constructor(_nome: string, _lista_de_atrações: Atracao){
+    constructor(_nome: string, _lista_de_atracoes: Array<Atracao>){
         this.nome = _nome;
-        this.lista_de_atrações = _lista_de_atrações;
+        this.lista_de_atracoes = _lista_de_atracoes;
     }
 
     public setNome(nome: string): void {
@@ -17,10 +18,10 @@ export class Zoologico {
         return this.nome;
     }
 
-    public setLista_de_atrações(lista_de_atrações: Atracao):void{
-        this.lista_de_atrações = lista_de_atrações;
+    public setLista_de_atrações(lista_de_atracoes: Array<Atracao>):void{
+        this.lista_de_atracoes = lista_de_atracoes;
     }
-    public getLista_de_atrações():Atracao{
-        return this.lista_de_atrações
+    public getLista_de_atrações():Array<Atracao>{
+        return this.lista_de_atracoes
     }
 }
